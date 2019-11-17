a372decf6cc1458e0321d5051cd961f60e6e3216
xiahangli <2427417167@qq.com>
2019-11-17 17:58:57 +0800

    添加图片资源

--- cmakedemo/src/main/cpp/native-lib.cpp
+++ cmakedemo/src/main/cpp/native-lib.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <opencv2/opencv.hpp>
+#include <android/log.h>
 
 using namespace cv;
 using namespace std;
@@ -65,10 +66,12 @@ JNIEXPORT jintArray JNICALL
 Java_com_example_cmakedemo_MainActivity_getGray(JNIEnv *env, jobject instance, jintArray buf_,
                                                 jint w, jint h) {
     jint *buf = env->GetIntArrayElements(buf_, NULL);
-
+    jint x=0,y;
     if (buf == NULL)return 0;
     Mat imgData(h, w, CV_8UC4, (unsigned char *) buf);//创建Mat矩阵对象
 
+
+//    __android_log_print(ANDROID_LOG_ERROR,"test",A.to);
     uchar *ptr = imgData.ptr(0);
     for (int i = 0; i < w * h; i++) {
         //计算公式：Y(亮度) = 0.299*R + 0.587*G + 0.114*B
@@ -82,6 +85,46 @@ Java_com_example_cmakedemo_MainActivity_getGray(JNIEnv *env, jobject instance, j
     int size = w * h;
     jintArray result = env->NewIntArray(size);
     env->SetIntArrayRegion(result, 0, size, buf);
+
     env->ReleaseIntArrayElements(buf_, buf, 0);
     return result;
 }
+
+extern "C"
+JNIEXPORT jintArray JNICALL Java_com_example_cmakedemo_MainActivity_roi(JNIEnv *env, jobject obj1, jintArray buf_, jint w, jint h){
+
+    jint *cbuf;
+    cbuf = env->GetIntArrayElements(buf_, JNI_FALSE);
+    if (cbuf == NULL) {
+        return 0;
+    }
+
+    Mat srcImage(h, w, CV_8UC4, (unsigned char *) cbuf);//构造函数
+
+    //拷贝需要复制的图像区域
+    Mat partImage;
+    srcImage(Rect(0, 0, 400, 400)).copyTo(partImage);//拷贝矩阵，开辟内存 src.copyTo(dst)，将src复制到dst矩阵中
+
+    //得到圈出ROI的Mat
+    Mat ROIImage = srcImage(Rect(100, 100, 400, 400));
+
+//Ptr
+    //加载掩模,必须是灰度图或二值图
+//    Mat mask;
+//    cvtColor(partImage, mask, COLOR_BGRA2GRAY);//颜色转换成灰色的图像
+
+//    //对roi子图像进行修改
+//    Sobel(ROIImage,ROIImage,1,0,3);//src和dst为同一个
+    //合并
+    partImage.copyTo(ROIImage);//复制图像到另一个图像或矩阵上，可选参数是掩码
+
+    jint* ptr = ROIImage.ptr<jint>(0);
+
+    int size = w * h;
+
+    jintArray result = env->NewIntArray(size);
+    env->SetIntArrayRegion(result, 0, size, ptr);//将ptr指向的srcImage对象像素数值赋值给result
+    env->ReleaseIntArrayElements(buf_, cbuf, 0);
+    return result;
+}
+
--- cmakedemo/src/main/java/com/example/cmakedemo/MainActivity.java
+++ cmakedemo/src/main/java/com/example/cmakedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cmakedemo;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.opengl.GLSurfaceView;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -17,29 +19,52 @@ public class MainActivity extends AppCompatActivity {
 
     public native String getC();
 
-    public native int[] getGray(int buf[],int w,int h);
+    public native int[] getGray(int buf[], int w, int h);
+
+
+    public native int[] roi(int buf[], int w, int h);
 
     private ImageView mImageView;
+    private ImageView imageView1;
+    private ImageView imageView2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView  = findViewById(R.id.image_view);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
-                        R.mipmap.ic_launcher_round)).getBitmap();
-                int w = bitmap.getWidth(), h = bitmap.getHeight();
-                int[] pix = new int[w * h];
-                bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-                int[] resultPixels = getGray(pix,w,h);
-                Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
-                result.setPixels(resultPixels, 0, w, 0, 0,w, h);
-                mImageView.setImageBitmap(result);
-            }
-        },2000);
+        mImageView = findViewById(R.id.image_view);
+        imageView1 = findViewById(R.id.image_view1);
+        imageView2 = findViewById(R.id.image_view2);
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
+                R.drawable.test)).getBitmap();
+        int w = bitmap.getWidth(), h = bitmap.getHeight();
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+        int[] resultPixels = getGray(pix,w,h);
+        Bitmap result = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        result.setPixels(resultPixels, 0, w, 0, 0, w, h);
+        Matrix matrix = new Matrix();
+//        matrix.postScale()
+
+        mImageView.setImageBitmap(result);
+//
+//        imageView1.setImageBitmap(result);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
+//                        R.drawable.test)).getBitmap();
+//                int w = bitmap.getWidth(), h = bitmap.getHeight();
+//                int[] pix = new int[w * h];
+//                bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+//                int[] resultPixels = getGray(pix,w,h);
+//                Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
+//                result.setPixels(resultPixels, 0, w, 0, 0,w, h);
+//                mImageView.setImageBitmap(result);
+//            }
+//        },2000);
     }
 
     @Override
