2f1f69f88e4c7914d62318d372232223fcac14dd
xiahangli <2427417167@qq.com>
2020-03-28 05:03:24 +0800

    oepncv code

---
+++ cmakedemo/src/main/cpp/include/lib_log/LogUtil.h
@@ -0,0 +1,22 @@
+//
+// Created by glumes on 2017/5/26.
+//
+
+#ifndef ANDROIDCPPSOLIB_LOGUTIL_H
+#define ANDROIDCPPSOLIB_LOGUTIL_H
+
+
+#include <string>
+
+using namespace std;
+
+class LogUtil {
+
+public:
+    LogUtil();
+
+    void test(string msg);
+};
+
+
+#endif //ANDROIDCPPSOLIB_LOGUTIL_H
