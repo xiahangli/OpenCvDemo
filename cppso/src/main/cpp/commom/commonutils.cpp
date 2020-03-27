f3d1bbbe9649f892cca1db568f907e5317ca81b0
xiahangli <2427417167@qq.com>
2020-03-28 06:29:54 +0800

    keyong banben

--- cppso/src/main/cpp/commom/commonutils.cpp
+++ cppso/src/main/cpp/commom/commonutils.cpp
@@ -6,13 +6,13 @@
 #include "commonutils.h"
 
 //commonutils类的抛出异常的实现
-void commonutils::throwmsgbyname(JNIEnv *env, const char name, const char *msg) {
-    //1.找到类
-    jclass clz = env->FindClass(&name);
-    //2.调用核心的ThrowNew，在c++层抛出java的异常
-    if(clz!=NULL){
-        env->ThrowNew(clz,msg);
-    }
-    //3.释放开辟的local内存区域DeleteXXXReference
-    env->DeleteLocalRef(clz);
-}
+//void commonutils::throwmsgbyname(JNIEnv *env, const char name, const char *msg) {
+//    //1.找到类
+//    jclass clz = env->FindClass(&name);
+//    //2.调用核心的ThrowNew，在c++层抛出java的异常
+//    if(clz!=NULL){
+//        env->ThrowNew(clz,msg);
+//    }
+//    //3.释放开辟的local内存区域DeleteXXXReference
+//    env->DeleteLocalRef(clz);
+//}
